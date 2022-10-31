68a958ad8ebabd2d324e12293bbd63c19c299377
Bastien DUMONT <bastienxs@gmail.com>
2022-10-31 22:15:34 +0100

    base config

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nest Lesson management
+
+## Create container
+
+`docker run --name mongo -p 27017:27017 -d mongo`
---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { LessonModule } from './lesson/lesson.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { LessonResolver } from './lesson/lesson.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+  ],
+})
+export class AppModule {}
